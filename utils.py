b328190380b04bc21dcf89225044545030712d61
shiyani21 <shiyani@seas.upenn.edu>
2021-10-30 12:48:21 -0400

    iou added

--- utils.py
+++ utils.py
@@ -12,6 +12,42 @@ def IOU(boxA, boxB):
     ##################################
     #TODO compute the IOU between the boxA, boxB boxes
     ##################################
+    if boxA.shape == (4,):
+        boxA = boxA.reshape(1,-1)
+    if boxB.shape == (4,):
+        boxB = boxB.reshape(1,-1)
+    
+    # compute the range of the anchor box
+    x_a_min = (boxA[:,0] - boxA[:,2]/2).reshape(-1,1)
+    x_a_max = (boxA[:,0] + boxA[:,2]/2).reshape(-1,1)
+    
+    y_a_min = (boxA[:,1] - boxA[:,3]/2).reshape(-1,1)
+    y_a_max = (boxA[:,1] + boxA[:,3]/2).reshape(-1,1)
+    # compute the anchor area
+    area_A = (boxA[:,2] * boxA[:,3]).reshape(-1,1)
+    
+    # compute the range of the gt bounding box
+    x_b_min = (boxB[:,0] - boxB[:,2]/2).reshape(-1,1)
+    x_b_max = (boxB[:,0] + boxB[:,2]/2).reshape(-1,1)
+    y_b_min = (boxBt[:,1] - boxB[:,3]/2).reshape(-1,1)
+    y_b_max = (boxB[:,1] + boxB[:,3]/2).reshape(-1,1)
+    # compute the gt area
+    area_B = (boxB[:,2] * boxB[:,3]).reshape(-1,1)
+    
+    # print(x_p_max)
+    # print(x_gt_max.T)
+    # print(np.minimum(x_p_max, x_gt_max.T))
+    # compute the intersect w & h
+    intersect_a_b_w = np.maximum(np.minimum(x_a_max, x_b_max.T) - np.maximum(x_a_min, x_b_min.T), 0)  
+    intersect_a_b_h = np.maximum(np.minimum(y_a_max, y_b_max.T) - np.maximum(y_a_min, y_b_min.T), 0)
+    # compute intersect area
+    area_intersect = intersect_a_b_w * intersect_a_b_h
+    
+    # compute union area
+    area_union = (area_A + area_B.T) - area_intersect
+    # print("Area of intersect: ", area_intersect)
+    # print("Area of Union: ", area_union)
+    iou = area_intersect / area_union
     return iou
 
 
